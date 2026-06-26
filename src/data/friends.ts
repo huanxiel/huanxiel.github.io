0209a75ce080e2af6269c47ee2421db15a6f7872
huanxiel <1492841662@qq.com>
2026-06-27 04:28:39 +0800

    Rebuild blog with Astro

---
+++ src/data/friends.ts
@@ -0,0 +1,8 @@
+export const friends = [
+  "https://mylyn.cn",
+  "https://Kendal-ming.github.io",
+  "http://blog.zerohzzzz.top",
+  "https://unclezhou.top",
+  "https://herveyb3b4.github.io",
+  "https://salt114514.github.io/"
+];
---
+++ src/data/site.ts
@@ -0,0 +1,9 @@
+export const site = {
+  title: "LMF",
+  description: "A blog for long-form posts, notes, and future projects.",
+  nav: [
+    { href: "/", label: "Home" },
+    { href: "/links", label: "Links" },
+    { href: "/about", label: "About" }
+  ]
+};
